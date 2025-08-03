2a6042a185e18729b39b7b77c747ee844f2b242f
HashirA32 <hashirofficiala32@gmail.com>
2025-08-03 06:06:02 -0700

    added signIn and signUp pages logic

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -64,7 +64,7 @@ const SignIn = () => {
       <Card className="w-[350px] p-5 mb-5 ">
         <div className="flex flex-col text-2xl font-bold text-center mb-5">
           Good to See You Again!
-          <span className="text-[#8B5CF6]"> Let’s get you signed in.</span>
+          <span className="text-[#8B5CF6]"> Let’s get you signed in</span>
         </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
