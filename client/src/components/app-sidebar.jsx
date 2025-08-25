e74d3c706c448e22471b0eaa0808df1ee1725bb2
HashirA32 <hashirofficiala32@gmail.com>
2025-08-25 08:07:34 -0700

    Added category list in side-bar

--- client/src/components/app-sidebar.jsx
+++ client/src/components/app-sidebar.jsx
@@ -18,8 +18,23 @@ import {
   RouteProduct,
 } from "./Helpers/RouteNames";
 import { Link } from "react-router-dom";
+import { useFetch } from "@/hooks/UseFetch";
+import { getEnv } from "@/components/Helpers/getenv";
+import { BiSolidCategoryAlt } from "react-icons/bi";
+import { IoHome } from "react-icons/io5";
+import { RiAlignItemBottomLine } from "react-icons/ri";
+import { BiUser } from "react-icons/bi";
+import { TbSettings } from "react-icons/tb";
 
 const AppSidebar = () => {
+  const { data: CategoryData } = useFetch(
+    `${getEnv("VITE_API_BASE_URL")}/category/all-categories`,
+    {
+      method: "get",
+      credentials: "include",
+    }
+  );
+
   return (
     <Sidebar>
       <SidebarContent>
@@ -30,7 +45,10 @@ const AppSidebar = () => {
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link to={RouteIndex}>
-                    <span className="font-semibold text-[1rem]">Home</span>
+                    <span className="font-semibold text-[1rem] flex gap-1 items-center justify-center">
+                      <IoHome />
+                      Home
+                    </span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
@@ -38,7 +56,10 @@ const AppSidebar = () => {
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link to={RouteProduct}>
-                    <span className="font-semibold text-[1rem]">Products</span>
+                    <span className="font-semibold text-[1rem] flex gap-1 items-center justify-center">
+                      <RiAlignItemBottomLine />
+                      Products
+                    </span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
@@ -46,8 +67,8 @@ const AppSidebar = () => {
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link to={RouteCategoryDetails}>
-                    <span className="font-semibold text-[1rem]">
-                      Categories
+                    <span className="font-semibold text-[1rem] flex gap-1 items-center justify-center">
+                      <BiSolidCategoryAlt /> Categories
                     </span>
                   </Link>
                 </SidebarMenuButton>
@@ -56,7 +77,10 @@ const AppSidebar = () => {
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link href="#">
-                    <span className="font-semibold text-[1rem]">Users</span>
+                    <span className="font-semibold text-[1rem] flex gap-1 items-center justify-center">
+                      <BiUser className="font-extralight" />
+                      Users
+                    </span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
@@ -64,13 +88,38 @@ const AppSidebar = () => {
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link href="#">
-                    <span className="font-semibold text-[1rem]">Settings</span>
+                    <span className="font-semibold text-[1rem] flex gap-1 items-center justify-center">
+                      <TbSettings />
+                      Settings
+                    </span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        <SidebarGroupContent>
+          <SidebarGroupLabel>List of category</SidebarGroupLabel>
+          <SidebarMenu>
+            {CategoryData &&
+              CategoryData.categories.length > 0 &&
+              CategoryData.categories.map((category) => {
+                return (
+                  <SidebarMenuItem key={category._id}>
+                    <SidebarMenuButton asChild>
+                      <Link href="#">
+                        <span className="flex gap-1 items-center justify-center">
+                          <BiSolidCategoryAlt />
+
+                          {category.name}
+                        </span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+          </SidebarMenu>
+        </SidebarGroupContent>
       </SidebarContent>
     </Sidebar>
   );
